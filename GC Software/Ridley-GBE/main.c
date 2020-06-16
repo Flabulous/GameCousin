78fbeef4c32f368322f519adb9350eac25c980d2
Arandor <33958575+Flabulous@users.noreply.github.com>
2020-06-16 13:51:25 -0400

    Created RIDLEY emualtor

---
+++ GC Software/Ridley-GBE/main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+/*
+RIDLEY GAMEBOY EMULATOR
+VERSION 0.0.0
+*/
+int main()
+{
+    printf("RidleyGBEv000 Booting...\n");
+    return 0;
+}
