5bff730d5c95c717265233f8330a76313bbafaf4
Arandor <33958575+Flabulous@users.noreply.github.com>
2020-10-02 11:00:42 -0400

    Added memory management

--- GC Software/Ridley-GBE/win-env.c
+++ GC Software/Ridley-GBE/win-env.c
@@ -2,7 +2,8 @@
 #include <stdlib.h>
 #include <stdint.h>
 FILE *rom_file;
-char rom_mem[];
+*rom_mem;
+int SIZE;
 
 int winruntime() {
     printf("Ridley v0.01 Loaded.\n");
@@ -13,8 +14,7 @@ getrom:
     gets(input);
     int i = winopenrom(input);
     if (i == -1) {goto getrom;} else {winsetuprom();}
-
-
+    gets(input);
     return 0;
 }
 
@@ -26,21 +26,25 @@ int winopenrom(char nme[256])
         return -1;
     }
 
-    printf("File opened.");
+    printf("File opened.\n");
     return 0;
 }
 
 int winsetuprom()
 {
     fseek(rom_file, 0, SEEK_END);
-    int size = ftell(rom_file);
+    SIZE = ftell(rom_file);
     fseek(rom_file,0,SEEK_SET);
 
-    printf("Loading...");
-    for(int i=0; i<=size; i++) {
-        rom_mem[i] = fgetc(i);
+    rom_mem = malloc(SIZE);
+    printf("ROM is %d bytes in size, allocated %d bytes.", SIZE, rom_mem);
+
+    printf("Loading...\n");
+    for(int i=1; i<=SIZE; i++) {
+        *(rom_mem+i) = fgetc(rom_file);
+        //printf(" %x ",*(rom_mem+i));
     }
-    printf("Loaded.");
+    printf("\nLoaded.\n");
 
     return 0;
 }
