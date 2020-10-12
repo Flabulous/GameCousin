2e8be73d1ee393d2e02b7a9c308e64e58fa6373c
Arandor <33958575+Flabulous@users.noreply.github.com>
2020-10-12 14:38:34 -0400

    register rework

--- GC Software/Ridley-GBE/ridley.h
+++ GC Software/Ridley-GBE/ridley.h
@@ -10,20 +10,28 @@ unsigned char *rom_mem;
 unsigned char *ram_mem;
 unsigned char *vram_mem;
 
-union
-unsigned char a;
-unsigned char b;
-unsigned char c;
-unsigned char d;
-unsigned char e;
-unsigned char f;
-unsigned char h;
-unsigned char l;
-unsigned short
+//Registers (I think this will work)
+typedef union af {
+    unsigned char a;
+    unsigned char f;
+    unsigned short af;
 
-
-
-
+};
+typedef union bc {
+    unsigned char b;
+    unsigned char c;
+    unsigned short bc;
+};
+typedef union de {
+    unsigned char d;
+    unsigned char e;
+    unsigned short de;
+};
+typedef union hl {
+    unsigned char h;
+    unsigned char l;
+    unsigned short hl;
+};
 //Opcodes
 unsigned char *ops_tbl;
 
